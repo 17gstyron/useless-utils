07e7a2c339d28afb911b278f19f117909ad30263
17gstyron <17gstyron@gmail.com>
2020-11-12 09:23:23 -0600

    Fixed comment and indention of file

--- arrayToObjectConverter.js
+++ arrayToObjectConverter.js
@@ -1,20 +1,21 @@
-  /**
-   * Converts an array of objects into an object of with nested using unique property names from each
-   * @param {Array} arrayOfObjects Array of Objects to be converted into Object.
-   * @param {string} propertyName Name of property that contains a unique string or interger to use as property to access each object.
-   * @returns {Object} An Object with nested objects from the orginal Array.
-   */
-  const arrayToObjectConverter = (arrayOfObjects, propertyName) => {
-    const resultObj = {}
-    arrayOfObjects.forEach((object) => {
-      if (resultObj[object[propertyName]]) {
-        throw new Error(
-          `There are multiple Objects in the array with the Property of ${propertyName}.`,
-        )
-      }
-      resultObj[object[propertyName]] = object
-    })
-    return resultObj
-  }
+/**
+ * Converts an array of objects into an object of with nested using unique property names from each
+ * @param {Array} arrayOfObjects Array of Objects to be converted into Object.
+ * @param {string} propertyName Name of property that contains a unique string
+ * or interger to use as property to access each object.
+ * @returns {Object} An Object with nested objects from the orginal Array.
+ */
+const arrayToObjectConverter = (arrayOfObjects, propertyName) => {
+  const resultObj = {};
+  arrayOfObjects.forEach((object) => {
+    if (resultObj[object[propertyName]]) {
+      throw new Error(
+        `There are multiple Objects in the array with the Property of ${propertyName}.`
+      );
+    }
+    resultObj[object[propertyName]] = object;
+  });
+  return resultObj;
+};
 
-  export default arrayToObjectConverter
+export default arrayToObjectConverter;
