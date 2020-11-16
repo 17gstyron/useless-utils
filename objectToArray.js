22cb593dea3311f97da02f406cbd550fe7f55277
17gstyron <17gstyron@gmail.com>
2020-11-16 09:35:32 -0600

    added option param to allow preserving key names

--- objectToArray.js
+++ objectToArray.js
@@ -1,16 +1,21 @@
 /**
  * Converts an obeject into an array.
  * @param {Object} object Object to be converted into array.
+ * @param {Boolean} preserveKeys A boolean value that will preserve
+ *  keys by turning elements into objects and having properties for
+ *  both keys and values.
  * @returns {Array} An array built with values of object given.
  */
-const objectToArray = (object) => {
+const objectToArray = (object, preserveKeys) => {
   const resultArr = []
   for (const property in object) {
-    resultArr.push(object[property])
+    if (preserveKeys) {
+      resultArr.push({ key: property, value: object[property] })
+    } else {
+      resultArr.push(object[property])
+    }
   }
   return resultArr
 }
 
 module.exports.objectToArray = objectToArray
-
-console.log(objectToArray({ car: 'blue', age: 17, name: 'green' }))
