970ec6330ad808f5906bcf23aa085cf585d32eca
17gstyron <17gstyron@gmail.com>
2020-11-16 09:37:08 -0600

    updated javadocs for objectToArray

--- objectToArray.js
+++ objectToArray.js
@@ -1,12 +1,12 @@
 /**
  * Converts an obeject into an array.
  * @param {Object} object Object to be converted into array.
- * @param {Boolean} preserveKeys A boolean value that will preserve
+ * @param {Boolean} [preserveKeys] A boolean value that will preserve
  *  keys by turning elements into objects and having properties for
  *  both keys and values.
  * @returns {Array} An array built with values of object given.
  */
-const objectToArray = (object, preserveKeys) => {
+const objectToArray = (object, preserveKeys = null) => {
   const resultArr = []
   for (const property in object) {
     if (preserveKeys) {
